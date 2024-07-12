b078471d2a506f1511a9d8cb20ac765c7cd88b63
KalvieR <69376096+KalvieR@users.noreply.github.com>
2024-07-12 23:56:55 +0530

    Add files via upload

---
+++ project_hangman.py
@@ -0,0 +1,119 @@
+import random
+import string
+
+WORDLIST_FILENAME = "words.txt"
+
+def load_words():
+    """
+    returns: list, a list of valid words. Words are strings of lowercase letters.
+
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print("Loading word list from file...")
+    inFile = open(WORDLIST_FILENAME, 'r')
+    line = inFile.readline()
+    wordlist = line.split()
+    print(" ", len(wordlist), "words loaded.")
+    return wordlist
+
+def choose_word(wordlist):
+    """
+    wordlist (list): list of words (strings)
+
+    returns: a word from wordlist at random
+    """
+    return random.choice(wordlist)
+
+wordlist = load_words()
+def has_player_won(secret_word, letters_guessed):
+    """
+    secret_word: string, the lowercase word the user is guessing
+    letters_guessed: list (of lowercase letters), the letters that have been
+        guessed so far
+
+    returns: boolean, True if all the letters of secret_word are in letters_guessed,
+        False otherwise
+    """
+    for i in range(0,len(letters_guessed)):
+        if letters_guessed[i] in secret_word:
+            if i!=len(letters_guessed)-1:
+                continue
+            else:
+                return True
+
+def get_word_progress(secret_word, letters_guessed):
+    """
+    secret_word: string, the lowercase word the user is guessing
+    letters_guessed: list (of lowercase letters), the letters that have been
+        guessed so far
+
+    returns: string, comprised of letters and asterisks (*) that represents
+        which letters in secret_word have not been guessed so far
+    """
+    fin_res=''
+    for char in secret_word:
+        if char in letters_guessed:
+            fin_res=fin_res+char
+        else:
+            fin_res=fin_res+'*'
+    return fin_res
+
+def get_available_letters(letters_guessed):
+    """
+    letters_guessed: list (of lowercase letters), the letters that have been
+        guessed so far
+
+    returns: string, comprised of letters that represents which
+      letters have not yet been guessed. The letters should be returned in
+      alphabetical order
+    """
+
+def hangman(secret_word, with_help):
+    """
+    secret_word: string, the secret word to guess.
+    with_help: boolean, this enables help functionality if true.
+
+    Starts up an interactive game of Hangman.
+
+    * At the start of the game, let the user know how many
+      letters the secret_word contains and how many guesses they start with.
+
+    * The user should start with 10 guesses.
+
+    * Before each round, you should display to the user how many guesses
+      they have left and the letters that the user has not yet guessed.
+
+    * Ask the user to supply one guess per round. Remember to make
+      sure that the user puts in a single letter (or help character '!'
+      for with_help functionality)
+
+    * If the user inputs an incorrect consonant, then the user loses ONE guess,
+      while if the user inputs an incorrect vowel (a, e, i, o, u),
+      then the user loses TWO guesses.
+
+    * The user should receive feedback immediately after each guess
+      about whether their guess appears in the computer's word.
+
+    * After each guess, you should display to the user the
+      partially guessed word so far.
+
+    -----------------------------------
+    with_help functionality
+    -----------------------------------
+    * If the guess is the symbol !, you should reveal to the user one of the
+      letters missing from the word at the cost of 3 guesses. If the user does
+      not have 3 guesses remaining, print a warning message. Otherwise, add
+      this letter to their guessed word and continue playing normally.
+
+    Follows the other limitations detailed in the problem write-up.
+    """
+
+if __name__ == "__main__":
+
+    secret_word = choose_word(wordlist)
+    with_help = True
+    hangman(secret_word, with_help)
+
+    pass
+
